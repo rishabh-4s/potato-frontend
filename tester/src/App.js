feccbdd74e0c14f2d38a76a28e6f5caa3a406e1f
rishabh-4s <sukhijarishabh44@gmail.com>
2024-05-12 19:42:57 +0530

    css

--- tester/src/App.js
+++ tester/src/App.js
@@ -6,8 +6,10 @@ function App() {
   const [image, setImage] = useState(null);
   const [result, setResult] = useState(null);
   const [solutions, setSolutions] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleImageUpload = async (e) => {
+    setLoading(true);
     console.log('Uploading image...');
     const file = e.target.files[0];
     console.log('Selected file:', file);
@@ -27,21 +29,24 @@ function App() {
     } catch (error) {
       console.error('Error uploading image:', error);
       // Handle error here
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <div className="container">
-     <div><h1>Plant Disease Detector</h1></div>
+     <div class="heading"><h1>Plant Disease Detector</h1></div>
       <div className="upload-box">
-        <p>Upload your image</p>
+        {/* <p>Upload your image</p> */}
+        <p>{loading ? 'Loading...' : 'Upload your image'}</p>
         <label htmlFor="file-upload" className="custom-file-upload">
           Choose image
         </label>
         <input type="file" id="file-upload" onChange={handleImageUpload} />
       </div>
       {result && (
-        <div>
+        <div class="result">
           <h2>Result:</h2>
           <p>Disease: {result.class}</p>
           <h2>Control Measures:</h2>
